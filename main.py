c428ec48d615bdb17345fd8ea5e166f91fc4a395
Amandeep-Gupta <61717906+Amandeep-Gupta@users.noreply.github.com>
2020-11-17 13:53:30 -0800

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import json
 import os
 
 # Print to console/stdout
-print("Hello, {0}!".format(os.environ['DOMINO_PROJECT_OWNER']))
+# print("Hello, {0}!".format(os.environ['DOMINO_PROJECT_OWNER']))
 
 
 # Define a helper function to generate a random number
@@ -15,16 +15,19 @@ def random_number(start, stop):
     return random.uniform(start, stop)
 
 
-# Plot the values of random points
-x = random.sample(range(1000), 100)
-xbins = [0, len(x)]
-plt.bar(range(0, 100), x)
-plt.show()
-plt.savefig('results/myHistogramFromPython.png', format='png')
+print(random_number(1,1000))
 
-# Generate and save some key statistics to dominostats.json
-#   learn more at http://support.dominodatalab.com/hc/en-us/articles/204348169
-r2 = round(random_number(0, 1), 4)
-p = round(random_number(0, 1), 4)
-with open('dominostats.json', 'w') as f:
-    f.write(json.dumps({"R^2": r2, "p-value": p}))
+
+# # Plot the values of random points
+# x = random.sample(range(1000), 100)
+# xbins = [0, len(x)]
+# plt.bar(range(0, 100), x)
+# plt.show()
+# plt.savefig('results/myHistogramFromPython.png', format='png')
+
+# # Generate and save some key statistics to dominostats.json
+# #   learn more at http://support.dominodatalab.com/hc/en-us/articles/204348169
+# r2 = round(random_number(0, 1), 4)
+# p = round(random_number(0, 1), 4)
+# with open('dominostats.json', 'w') as f:
+#     f.write(json.dumps({"R^2": r2, "p-value": p}))
